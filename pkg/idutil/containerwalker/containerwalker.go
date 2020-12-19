1490313d6fb9ae55d78a8737ff74c7a65f5e3503
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-12-19 16:45:00 +0900

    idutil: new walker
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

---
+++ pkg/idutil/containerwalker/containerwalker.go
@@ -0,0 +1,70 @@
+/*
+   Copyright (C) nerdctl authors.
+   Copyright (C) containerd authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package containerwalker
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+
+	"github.com/AkihiroSuda/nerdctl/pkg/labels"
+	"github.com/containerd/containerd"
+)
+
+type Found struct {
+	Container  containerd.Container
+	Req        string // The raw request string. name, short ID, or long ID.
+	MatchIndex int    // Begins with 0, up to MatchCount - 1.
+	MatchCount int    // 1 on exact match. > 1 on ambiguous match. Never be <= 0.
+}
+
+type OnFound func(ctx context.Context, found Found) error
+
+type ContainerWalker struct {
+	Client  *containerd.Client
+	OnFound OnFound
+}
+
+// Walk walks containers and calls w.OnFound .
+// Req is name, short ID, or long ID.
+// Returns the number of the found entries.
+func (w *ContainerWalker) Walk(ctx context.Context, req string) (int, error) {
+	filters := []string{
+		fmt.Sprintf("labels.%q==%s", labels.Name, req),
+		fmt.Sprintf("id~=^%s.*$", regexp.QuoteMeta(req)),
+	}
+
+	containers, err := w.Client.Containers(ctx, filters...)
+	if err != nil {
+		return -1, err
+	}
+
+	matchCount := len(containers)
+	for i, c := range containers {
+		f := Found{
+			Container:  c,
+			Req:        req,
+			MatchIndex: i,
+			MatchCount: matchCount,
+		}
+		if e := w.OnFound(ctx, f); e != nil {
+			return -1, e
+		}
+	}
+	return matchCount, nil
+}
