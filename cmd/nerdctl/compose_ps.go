6a985250891b4bee9e11ad9d2f9d47a019ac01c0
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2021-10-12 16:33:03 +0900

    compose ps: decouple CLI from pkg/...
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- cmd/nerdctl/compose_ps.go
+++ cmd/nerdctl/compose_ps.go
@@ -17,7 +17,16 @@
 package main
 
 import (
-	"github.com/containerd/nerdctl/pkg/composer"
+	"bytes"
+	"fmt"
+	"text/tabwriter"
+	"text/template"
+
+	"github.com/containerd/containerd"
+	"github.com/containerd/nerdctl/pkg/formatter"
+	"github.com/containerd/nerdctl/pkg/labels"
+	"github.com/docker/cli/templates"
+	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
 )
 
@@ -39,6 +48,90 @@ func composePsAction(clicontext *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	po := composer.PsOptions{}
-	return c.Ps(ctx, clicontext, po, client)
+	services, err := c.Services(ctx)
+	if err != nil {
+		return err
+	}
+
+	type containerPrintable struct {
+		Name    string
+		Command string
+		Service string
+		Status  string
+		Ports   string
+	}
+
+	var containersPrintable []containerPrintable
+
+	for _, svc := range services {
+		for _, container := range svc.Containers {
+			containersGot, err := client.Containers(ctx, fmt.Sprintf("labels.%q==%s", labels.Name, container.Name))
+			if err != nil {
+				return err
+			}
+			if len(containersGot) != 1 {
+				return fmt.Errorf("expected 1 container, got %d", len(containersGot))
+			}
+			info, err := containersGot[0].Info(ctx, containerd.WithoutRefreshedMetadata)
+			if err != nil {
+				return err
+			}
+
+			spec, err := containersGot[0].Spec(ctx)
+			if err != nil {
+				return err
+			}
+			p := containerPrintable{
+				Name:    container.Name,
+				Command: formatter.InspectContainerCommandTrunc(spec),
+				Service: svc.Unparsed.Name,
+				Status:  "running", // FIXME
+				Ports:   formatter.FormatPorts(info.Labels),
+			}
+			containersPrintable = append(containersPrintable, p)
+		}
+	}
+
+	w := clicontext.App.Writer
+	var tmpl *template.Template
+	switch format := clicontext.String("format"); format {
+	case "", "table":
+		w = tabwriter.NewWriter(clicontext.App.Writer, 4, 8, 4, ' ', 0)
+		fmt.Fprintln(w, "NAME\tCOMMAND\tSERVICE\tSTATUS\tPORTS")
+	case "raw":
+		return errors.New("unsupported format: \"raw\"")
+	default:
+		var err error
+		tmpl, err = templates.Parse(format)
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, p := range containersPrintable {
+		if tmpl != nil {
+			var b bytes.Buffer
+			if err := tmpl.Execute(&b, p); err != nil {
+				return err
+			}
+			if _, err := fmt.Fprintf(w, b.String()+"\n"); err != nil {
+				return err
+			}
+		} else {
+			if _, err := fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n",
+				p.Name,
+				p.Command,
+				p.Service,
+				p.Status,
+				p.Ports,
+			); err != nil {
+				return err
+			}
+		}
+	}
+
+	if f, ok := w.(Flusher); ok {
+		return f.Flush()
+	}
+	return nil
 }
