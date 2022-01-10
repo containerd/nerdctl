57132f22ef6ae8dd6148f68737156b9e004a82b8
ningmingxiao <ning.mingxiao@zte.com.cn>
2022-01-10 22:59:25 +0800

    add add nerdctl run cpuset-mems,cpu-quota,cpu-period support
    Signed-off-by: ningmingxiao <ning.mingxiao@zte.com.cn>

--- cmd/nerdctl/run.go
+++ cmd/nerdctl/run.go
@@ -145,7 +145,10 @@ func setCreateFlags(cmd *cobra.Command) {
 		return []string{"host", "private"}, cobra.ShellCompDirectiveNoFileComp
 	})
 	cmd.Flags().String("cpuset-cpus", "", "CPUs in which to allow execution (0-3, 0,1)")
+	cmd.Flags().String("cpuset-mems", "", "MEMs in which to allow execution (0-3, 0,1)")
 	cmd.Flags().Int("cpu-shares", 0, "CPU shares (relative weight)")
+	cmd.Flags().Int64("cpu-quota", -1, "Limit CPU CFS (Completely Fair Scheduler) quota")
+	cmd.Flags().Uint64("cpu-period", 0, "Limit CPU CFS (Completely Fair Scheduler) period")
 	// device is defined as StringSlice, not StringArray, to allow specifying "--device=DEV1,DEV2" (compatible with Podman)
 	cmd.Flags().StringSlice("device", nil, "Add a host device to the container")
 	// ulimit is defined as StringSlice, not StringArray, to allow specifying "--ulimit=ULIMIT1,ULIMIT2" (compatible with Podman)
