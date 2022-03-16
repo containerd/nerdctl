1b50b46cffd290d4fe481504496fb5514877e2bb
click2cloud-lamda <click2cloud-lamda@click2cloud.net>
2022-03-16 18:30:32 +0530

    Fixed volume mount issue(issue 759) and bind propagation for Windows

--- pkg/mountutil/mountutil.go
+++ pkg/mountutil/mountutil.go
@@ -55,68 +55,144 @@ func ProcessFlagV(s string, volStore volumestore.VolumeStore) (*Processed, error
 	)
 
 	split := strings.Split(s, ":")
-	switch len(split) {
-	case 1:
-		dst = s
-		res.AnonymousVolume = idgen.GenerateID()
-		logrus.Debugf("creating anonymous volume %q, for %q", res.AnonymousVolume, s)
-		anonVol, err := volStore.Create(res.AnonymousVolume, []string{})
-		if err != nil {
-			return nil, err
-		}
-		src = anonVol.Mountpoint
-		res.Type = Volume
-	case 2, 3:
-		res.Type = Bind
-		src, dst = split[0], split[1]
-		if !strings.Contains(src, "/") {
-			// assume src is a volume name
-			vol, err := volStore.Get(src)
-			if err != nil {
-				if errors.Is(err, errdefs.ErrNotFound) {
-					vol, err = volStore.Create(src, nil)
-					if err != nil {
-						return nil, err
-					}
-				} else {
-					return nil, err
-				}
-			}
-			// src is now full path
-			src = vol.Mountpoint
-			res.Type = Volume
-		}
-		if !filepath.IsAbs(src) {
-			logrus.Warnf("expected an absolute path, got a relative path %q (allowed for nerdctl, but disallowed for Docker, so unrecommended)", src)
-			var err error
-			src, err = filepath.Abs(src)
+
+	if runtime.GOOS == "windows" {
+		switch len(split) {
+		case 2: //For anonymous volumes in Windows
+			dst = s
+			res.AnonymousVolume = idgen.GenerateID()
+			logrus.Debugf("creating anonymous volume %q, for %q", res.AnonymousVolume, s)
+			anonVol, err := volStore.Create(res.AnonymousVolume, []string{})
 			if err != nil {
 				return nil, err
 			}
-		}
-		if !filepath.IsAbs(dst) {
-			return nil, fmt.Errorf("expected an absolute path, got %q", dst)
-		}
-		rawOpts := ""
-		if len(split) == 3 {
-			rawOpts = split[2]
-		}
+			src = anonVol.Mountpoint
+			res.Type = Volume
+		case 3, 4, 5:
+			rawOpts := ""
+			res.Type = Bind
+			if len(split) == 3 {
+				src, dst = split[0], split[1]+":"+split[2]
+			} else if len(split) == 4 {
+				if strings.Contains(split[3], "\\") || strings.Contains(split[2], "/") {
+					src = split[0] + ":" + split[1]
+					dst = split[2] + ":" + split[3]
+				} else {
+					src = split[0]
+					dst = split[1] + ":" + split[2]
+					rawOpts = split[3]
+				}
 
-		// always call parseVolumeOptions for bind mount to allow the parser to add some default options
-		var err error
-		var specOpts []oci.SpecOpts
-		options, specOpts, err = parseVolumeOptions(res.Type, src, rawOpts)
-		if err != nil {
-			return nil, err
+			} else if len(split) == 5 {
+				src = split[0] + ":" + split[1]
+				dst = split[2] + ":" + split[3]
+				rawOpts = split[4]
+			}
+			if !strings.Contains(src, "/") && !strings.Contains(src, "\\") {
+				// assume src is a volume name
+				vol, err := volStore.Get(src)
+				if err != nil {
+					if errors.Is(err, errdefs.ErrNotFound) {
+						vol, err = volStore.Create(src, nil)
+						if err != nil {
+							return nil, err
+						}
+						src = vol.Mountpoint
+						res.Type = Volume
+					} else {
+						return nil, err
+					}
+				} else {
+					// src is now full path
+					src = vol.Mountpoint
+				}
+			}
+			if !filepath.IsAbs(src) {
+				logrus.Warnf("expected an absolute path, got a relative path %q (allowed for nerdctl, but disallowed for Docker, so unrecommended)", src)
+				var err error
+				src, err = filepath.Abs(src)
+				if err != nil {
+					return nil, err
+				}
+			}
+			if !filepath.IsAbs(dst) {
+				return nil, fmt.Errorf("expected an absolute path, got %q", dst)
+			}
+
+			// always call parseVolumeOptions for bind mount to allow the parser to add some default options
+			var err error
+			var specOpts []oci.SpecOpts
+			options, specOpts, err = parseVolumeOptions(res.Type, src, rawOpts)
+			if err != nil {
+				return nil, err
+			}
+			res.Opts = append(res.Opts, specOpts...)
+		default:
+			return nil, fmt.Errorf("failed to parse %q", s)
 		}
-		res.Opts = append(res.Opts, specOpts...)
-	default:
-		return nil, fmt.Errorf("failed to parse %q", s)
+	} else {
+		switch len(split) {
+		case 1:
+			dst = s
+			res.AnonymousVolume = idgen.GenerateID()
+			logrus.Debugf("creating anonymous volume %q, for %q", res.AnonymousVolume, s)
+			anonVol, err := volStore.Create(res.AnonymousVolume, []string{})
+			if err != nil {
+				return nil, err
+			}
+			src = anonVol.Mountpoint
+			res.Type = Volume
+		case 2, 3:
+			res.Type = Bind
+			src, dst = split[0], split[1]
+			if !strings.Contains(src, "/") {
+				// assume src is a volume name
+				vol, err := volStore.Get(src)
+				if err != nil {
+					if errors.Is(err, errdefs.ErrNotFound) {
+						vol, err = volStore.Create(src, nil)
+						if err != nil {
+							return nil, err
+						}
+					} else {
+						return nil, err
+					}
+				}
+				// src is now full path
+				src = vol.Mountpoint
+				res.Type = Volume
+			}
+			if !filepath.IsAbs(src) {
+				logrus.Warnf("expected an absolute path, got a relative path %q (allowed for nerdctl, but disallowed for Docker, so unrecommended)", src)
+				var err error
+				src, err = filepath.Abs(src)
+				if err != nil {
+					return nil, err
+				}
+			}
+			if !filepath.IsAbs(dst) {
+				return nil, fmt.Errorf("expected an absolute path, got %q", dst)
+			}
+			rawOpts := ""
+			if len(split) == 3 {
+				rawOpts = split[2]
+			}
+
+			// always call parseVolumeOptions for bind mount to allow the parser to add some default options
+			var err error
+			var specOpts []oci.SpecOpts
+			options, specOpts, err = parseVolumeOptions(res.Type, src, rawOpts)
+			if err != nil {
+				return nil, err
+			}
+			res.Opts = append(res.Opts, specOpts...)
+		default:
+			return nil, fmt.Errorf("failed to parse %q", s)
+		}
 	}
-
 	fstype := "nullfs"
 	if runtime.GOOS != "freebsd" {
-		fstype = "none"
+		fstype = ""
 		options = append(options, "rbind")
 	}
 	res.Mount = specs.Mount{
--- pkg/mountutil/mountutil_windows.go
+++ pkg/mountutil/mountutil_windows.go
@@ -17,13 +17,16 @@
 package mountutil
 
 import (
+	"context"
 	"fmt"
-	"strings"
-
+	"github.com/containerd/containerd/containers"
 	"github.com/containerd/containerd/errdefs"
+	"github.com/containerd/containerd/mount"
 	"github.com/containerd/containerd/oci"
 	"github.com/containerd/nerdctl/pkg/mountutil/volumestore"
 	"github.com/sirupsen/logrus"
+	"path/filepath"
+	"strings"
 )
 
 func getUnprivilegedMountFlags(path string) ([]string, error) {
@@ -38,31 +41,195 @@ const DefaultPropagationMode = ""
 
 // parseVolumeOptions parses specified optsRaw with using information of
 // the volume type and the src directory when necessary.
+//func parseVolumeOptions(vType, src, optsRaw string) ([]string, []oci.SpecOpts, error) {
+//	var writeModeRawOpts []string
+//	for _, opt := range strings.Split(optsRaw, ",") {
+//		switch opt {
+//		case "rw":
+//			writeModeRawOpts = append(writeModeRawOpts, opt)
+//		case "ro":
+//			writeModeRawOpts = append(writeModeRawOpts, opt)
+//		case "":
+//			// NOP
+//		default:
+//			logrus.Warnf("unsupported volume option %q", opt)
+//		}
+//	}
+//	var opts []string
+//	if len(writeModeRawOpts) > 1 {
+//		return nil, nil, fmt.Errorf("duplicated read/write volume option: %+v", writeModeRawOpts)
+//	} else if len(writeModeRawOpts) > 0 && writeModeRawOpts[0] == "ro" {
+//		opts = append(opts, "ro")
+//	} // No need to return option when "rw"
+//	return opts, nil, nil
+//}
+//
+
+func ProcessFlagTmpfs(s string) (*Processed, error) {
+	return nil, errdefs.ErrNotImplemented
+}
+
 func parseVolumeOptions(vType, src, optsRaw string) ([]string, []oci.SpecOpts, error) {
-	var writeModeRawOpts []string
+	return parseVolumeOptionsWithMountInfo(vType, src, optsRaw, getMountInfo)
+}
+
+// getMountInfo gets mount.Info of a directory.
+func getMountInfo(dir string) (mount.Info, error) {
+	sourcePath, err := filepath.EvalSymlinks(dir)
+	if err != nil {
+		return mount.Info{}, err
+	}
+	return mount.Lookup(sourcePath)
+}
+
+// parseVolumeOptionsWithMountInfo is the testable implementation
+// of parseVolumeOptions.
+func parseVolumeOptionsWithMountInfo(vType, src, optsRaw string, getMountInfoFunc func(string) (mount.Info, error)) ([]string, []oci.SpecOpts, error) {
+	var (
+		writeModeRawOpts   []string
+		propagationRawOpts []string
+	)
 	for _, opt := range strings.Split(optsRaw, ",") {
 		switch opt {
-		case "rw":
+		case "rw", "ro", "rro":
 			writeModeRawOpts = append(writeModeRawOpts, opt)
-		case "ro":
-			writeModeRawOpts = append(writeModeRawOpts, opt)
+		case "private", "rprivate", "shared", "rshared", "slave", "rslave":
+			propagationRawOpts = append(propagationRawOpts, opt)
 		case "":
 			// NOP
 		default:
 			logrus.Warnf("unsupported volume option %q", opt)
 		}
 	}
+
 	var opts []string
+	var specOpts []oci.SpecOpts
+
 	if len(writeModeRawOpts) > 1 {
 		return nil, nil, fmt.Errorf("duplicated read/write volume option: %+v", writeModeRawOpts)
-	} else if len(writeModeRawOpts) > 0 && writeModeRawOpts[0] == "ro" {
-		opts = append(opts, "ro")
-	} // No need to return option when "rw"
-	return opts, nil, nil
+	} else if len(writeModeRawOpts) > 0 {
+		switch writeModeRawOpts[0] {
+		case "ro":
+			opts = append(opts, "ro")
+		case "rro":
+			// Mount option "rro" is supported since crun v1.4 / runc v1.1 (https://github.com/opencontainers/runc/pull/3272), with kernel >= 5.12.
+			// Older version of runc just ignores "rro", so we have to add "ro" too, to our best effort.
+			opts = append(opts, "ro", "rro")
+			if len(propagationRawOpts) != 1 || propagationRawOpts[0] != "rprivate" {
+				logrus.Warn("Mount option \"rro\" should be used in conjunction with \"rprivate\"")
+			}
+		case "rw":
+			// NOP
+		default:
+			// NOTREACHED
+			return nil, nil, fmt.Errorf("unexpected writeModeRawOpts[0]=%q", writeModeRawOpts[0])
+		}
+	}
+
+	if len(propagationRawOpts) > 1 {
+		return nil, nil, fmt.Errorf("duplicated volume propagation option: %+v", propagationRawOpts)
+	} else if len(propagationRawOpts) > 0 && vType != Bind {
+		return nil, nil, fmt.Errorf("volume propagation option is only supported for bind mount: %+v", propagationRawOpts)
+	} else if vType == Bind {
+		var pFlag string
+		var got string
+		if len(propagationRawOpts) > 0 {
+			got = propagationRawOpts[0]
+		}
+		switch got {
+		case "shared", "rshared":
+			pFlag = got
+			// a bind mount can be shared from shared mount
+			mi, err := getMountInfoFunc(src)
+			if err != nil {
+				return nil, nil, err
+			}
+			if err := ensureMountOptionalValue(mi, "shared:"); err != nil {
+				return nil, nil, err
+			}
+
+			// NOTE: Though OCI Runtime Spec doesn't explicitly describe, runc's default
+			//       of RootfsPropagation is unix.MS_SLAVE | unix.MS_REC (i.e. runc applies
+			//       "slave" to all mount points in the container recursively). This ends
+			//       up marking the bind src directories "slave" and preventing it to shared
+			//      with the host. So we set RootfsPropagation to "shared" here.
+			//
+			// See also:
+			// - OCI Runtime Spec: https://github.com/opencontainers/runtime-spec/blob/v1.0.2/config-linux.md#rootfs-mount-propagation
+			// - runc implementation: https://github.com/opencontainers/runc/blob/v1.0.0/libcontainer/rootfs_linux.go#L771-L777
+			specOpts = append(specOpts, func(ctx context.Context, cli oci.Client, c *containers.Container, s *oci.Spec) error {
+				switch s.Linux.RootfsPropagation {
+				case "shared", "rshared":
+					// NOP
+				default:
+					s.Linux.RootfsPropagation = "shared"
+				}
+				return nil
+			})
+		case "slave", "rslave":
+			pFlag = got
+			// a bind mount can be a slave of shared or an existing slave mount
+			mi, err := getMountInfoFunc(src)
+			if err != nil {
+				return nil, nil, err
+			}
+			if err := ensureMountOptionalValue(mi, "shared:", "master:"); err != nil {
+				return nil, nil, err
+			}
+
+			// See above comments about RootfsPropagation. Here we make sure that
+			// the mountpoint can be a slave of the host mount.
+			specOpts = append(specOpts, func(ctx context.Context, cli oci.Client, c *containers.Container, s *oci.Spec) error {
+				switch s.Linux.RootfsPropagation {
+				case "shared", "rshared", "slave", "rslave":
+					// NOP
+				default:
+					s.Linux.RootfsPropagation = "rslave"
+				}
+				return nil
+			})
+		case "private", "rprivate":
+			pFlag = got
+		default:
+			// No propagation is specified to this bind mount.
+			// NOTE: When RootfsPropagation is set (e.g. by other bind mount option), that
+			//       propagation mode will be applied to this bind mount as well. So we need
+			//       to set "rprivate" explicitly for preventing this bind mount from unexpectedly
+			//       shared with the host. This behaviour is compatible to docker:
+			//       https://github.com/moby/moby/blob/v20.10.7/volume/mounts/linux_parser.go#L320-L322
+			//
+			// TODO: directories managed by containerd (e.g. /var/lib/containerd, /run/containerd, ...)
+			//       should be marked as "rslave" instead of "rprivate". This is because allowing
+			//       containers to hold their private bind mounts will prevent containerd from remove
+			//       them. See also: https://github.com/moby/moby/pull/36055.
+			//       Unfortunately, containerd doesn't expose the locations of directories where it manages.
+			//       Current workaround is explicitly add "rshared" or "rslave" option to these bind mounts.
+			pFlag = DefaultPropagationMode
+		}
+		opts = append(opts, pFlag)
+	}
+
+	return opts, specOpts, nil
 }
 
-func ProcessFlagTmpfs(s string) (*Processed, error) {
-	return nil, errdefs.ErrNotImplemented
+// ensure the mount of the specified directory has either of the specified
+// "optional" value in the entry in the /proc/<pid>/mountinfo file.
+//
+// For more details about "optional" field:
+// - https://github.com/moby/sys/blob/mountinfo/v0.4.1/mountinfo/mountinfo.go#L52-L56
+func ensureMountOptionalValue(mi mount.Info, vals ...string) error {
+	var hasValue bool
+	for _, opt := range strings.Split(mi.Optional, " ") {
+		for _, mark := range vals {
+			if strings.HasPrefix(opt, mark) {
+				hasValue = true
+			}
+		}
+	}
+	if !hasValue {
+		return fmt.Errorf("mountpoint %q doesn't have optional field neither of %+v", mi.Mountpoint, vals)
+	}
+	return nil
 }
 
 func ProcessFlagMount(s string, volStore volumestore.VolumeStore) (*Processed, error) {
