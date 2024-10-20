94307a538976b961f1894221f575dff9b67da60a
apostasie <spam_blackhole@farcloser.world>
2024-10-20 13:27:13 -0700

    Rewrite container diff test
    Signed-off-by: apostasie <spam_blackhole@farcloser.world>

---
+++ cmd/nerdctl/container/container_diff_test.go
@@ -0,0 +1,59 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package container
+
+import (
+	"testing"
+
+	"github.com/containerd/nerdctl/v2/pkg/testutil"
+	"github.com/containerd/nerdctl/v2/pkg/testutil/nerdtest"
+	"github.com/containerd/nerdctl/v2/pkg/testutil/test"
+)
+
+func TestDiff(t *testing.T) {
+	testCase := nerdtest.Setup()
+
+	// It is unclear why this is failing with docker when run in parallel
+	// Obviously some other container test is interfering
+	if nerdtest.IsDocker() {
+		testCase.NoParallel = true
+	}
+
+	testCase.Require = test.Not(test.Windows)
+
+	testCase.Setup = func(data test.Data, helpers test.Helpers) {
+		helpers.Ensure("run", "-d", "--name", data.Identifier(), testutil.CommonImage,
+			"sh", "-euxc", "touch /a; touch /bin/b; rm /bin/base64")
+	}
+
+	testCase.Cleanup = func(data test.Data, helpers test.Helpers) {
+		helpers.Anyhow("rm", "-f", data.Identifier())
+	}
+
+	testCase.Command = func(data test.Data, helpers test.Helpers) test.TestableCommand {
+		return helpers.Command("diff", data.Identifier())
+	}
+
+	testCase.Expected = test.Expects(0, nil, test.All(
+		test.Contains("A /a"),
+		test.Contains("C /bin"),
+		test.Contains("A /bin/b"),
+		test.Contains("D /bin/base64"),
+	))
+
+	testCase.Run(t)
+}
