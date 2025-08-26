d20ab2bf8d79e34768b3ab94a28121cc9a043ca8
ChengyuZhu6 <hudson@cyzhu.com>
2025-08-26 14:51:09 +0800

    checkpoint: support checkpoint create command
    Signed-off-by: ChengyuZhu6 <hudson@cyzhu.com>

---
+++ cmd/nerdctl/checkpoint/checkpoint_create.go
@@ -0,0 +1,43 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package checkpoint
+
+import (
+	"github.com/containerd/nerdctl/v2/cmd/nerdctl/completion"
+	"github.com/spf13/cobra"
+)
+
+func CreateCommand() *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:               "create [OPTIONS] CONTAINER CHECKPOINT",
+		Short:             "Create a checkpoint from a running container",
+		Args:              cobra.ExactArgs(2),
+		RunE:              createAction,
+		ValidArgsFunction: createShellComplete,
+		SilenceUsage:      true,
+		SilenceErrors:     true,
+	}
+	return cmd
+}
+
+func createAction(cmd *cobra.Command, args []string) error {
+	return nil
+}
+
+func createShellComplete(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	return completion.ImageNames(cmd)
+}
