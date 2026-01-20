3fa39ae71a74fda5223152233ef1ab7db4c9a246
Joonsoo Won <wonjs0116@gmail.com>
2026-01-20 23:49:25 +0900

    test: refactor container_update_linux_test.go to use Tigron
    Signed-off-by: Joonsoo Won <wonjs0116@gmail.com>

--- cmd/nerdctl/container/container_update_linux_test.go
+++ cmd/nerdctl/container/container_update_linux_test.go
@@ -17,17 +17,53 @@
 package container
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/containerd/nerdctl/mod/tigron/expect"
+	"github.com/containerd/nerdctl/mod/tigron/require"
+	"github.com/containerd/nerdctl/mod/tigron/test"
+
 	"github.com/containerd/nerdctl/v2/pkg/testutil"
+	"github.com/containerd/nerdctl/v2/pkg/testutil/nerdtest"
 )
 
 func TestUpdateContainer(t *testing.T) {
-	testutil.DockerIncompatible(t)
-	testContainerName := testutil.Identifier(t)
-	base := testutil.NewBase(t)
-	base.Cmd("run", "-d", "--name", testContainerName, testutil.CommonImage, "sleep", "infinity").AssertOK()
-	defer base.Cmd("rm", "-f", testContainerName).Run()
-	base.Cmd("update", "--memory", "999999999", "--restart", "123", testContainerName).AssertFail()
-	base.Cmd("inspect", "--mode=native", testContainerName).AssertOutNotContains(`"limit": 999999999,`)
+	testCase := nerdtest.Setup()
+	testCase.Require = require.Not(nerdtest.Docker)
+
+	testCase.Setup = func(data test.Data, helpers test.Helpers) {
+		containerName := testutil.Identifier(t)
+		data.Labels().Set("containerName", containerName)
+		helpers.Ensure("run", "-d", "--name", containerName, testutil.CommonImage, "sleep", nerdtest.Infinity)
+		nerdtest.EnsureContainerStarted(helpers, containerName)
+	}
+
+	testCase.Cleanup = func(data test.Data, helpers test.Helpers) {
+		containerName := data.Labels().Get("containerName")
+		helpers.Anyhow("rm", "-f", containerName)
+	}
+
+	testCase.SubTests = []*test.Case{
+		{
+			Description: "should fail on unsupported restart policy value",
+			NoParallel:  true,
+			Command: func(data test.Data, helpers test.Helpers) test.TestableCommand {
+				containerName := data.Labels().Get("containerName")
+				return helpers.Command("update", "--memory", "999999999", "--restart", "123", containerName)
+			},
+			Expected: test.Expects(1, []error{errors.New("unsupported restart policy")}, nil),
+		},
+		{
+			Description: "should not update memory in inspect",
+			NoParallel:  true,
+			Command: func(data test.Data, helpers test.Helpers) test.TestableCommand {
+				containerName := data.Labels().Get("containerName")
+				return helpers.Command("inspect", "--mode=native", containerName)
+			},
+			Expected: test.Expects(expect.ExitCodeSuccess, nil, expect.DoesNotContain(`"limit": 999999999,`)),
+		},
+	}
+
+	testCase.Run(t)
 }
