96c2897901371242276aad65e0ee8978da6f1aea
Eugene Kalinin <e.v.kalinin@gmail.com>
2026-09-01 11:53:01 +0300

    feat(attachmux): add the attach socket transport
    Signed-off-by: Eugene Kalinin <e.v.kalinin@gmail.com>

---
+++ pkg/attachmux/socket_unsupported.go
@@ -0,0 +1,33 @@
+//go:build !(linux || freebsd)
+
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package attachmux
+
+import (
+	"context"
+	"net"
+)
+
+// Probe is not implemented on this platform, so callers keep the legacy path.
+func Probe(string) error { return ErrUnsupported }
+
+// Listen is not implemented on this platform.
+func Listen(string) (net.Listener, error) { return nil, ErrUnsupported }
+
+// Dial is not implemented on this platform.
+func Dial(context.Context, string) (*Session, error) { return nil, ErrUnsupported }
